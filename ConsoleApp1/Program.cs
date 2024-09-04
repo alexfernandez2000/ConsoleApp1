38fc5b76bd1873f8ae73d242f9079e1c4be234e5
mati <mati@DESKTOP-LBORILJ>
2024-09-04 11:13:16 +0200

    Comparador de palabras, division entera, potencias.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace ConsoleApp1
 {
@@ -13,7 +16,10 @@ namespace ConsoleApp1
                 option = ReadConsoleWord(@"Write the number of the exercice you want to execute:
 1: Lower/camel case
 2: Date comparation
-3: Palindrome");
+3: Palindrome
+4: Pow
+5: Enter number division
+6: Word comparer");
                 switch (option)
                 {
                     case "1":
@@ -25,6 +31,16 @@ namespace ConsoleApp1
                     case "3":
                         StartingPalindromeExcercice();
                         break;
+                    case "4":
+                        CalculatePowExercice();
+                        break;
+                    case "5":
+                        EnterNumberDivision();
+                        break;
+                    case "6":
+                        WordComparer();
+                        break;
+
                     default:
                         option = "end";
                         break;
@@ -36,6 +52,87 @@ namespace ConsoleApp1
 
             }
         }
+        #region Word Comparer
+        private static void WordComparer()
+        {
+            while (true)
+            {
+                if (ReadConsoleWord("Continue? Insert n to leave") == "n")
+                    break;
+                else
+                {
+                    string word = ReadConsoleWord("Word to compare");
+                    string comparedWord = ReadConsoleWord("Word to be compared");
+                    if (CompareWords(word,comparedWord))
+                        Console.WriteLine($"Word {word} is bigger or equals than {comparedWord}");
+                    else
+                        Console.WriteLine($"Word {comparedWord} is bigger than {word}");
+                }
+            }
+        }
+        private static bool CompareWords(string word, string comparedWord)
+        {
+            //Tiene en cuenta si es mayuscula o minuscula.
+                for (int index = 0; index < word.Length; index++)
+                {
+                    if (word[index] > comparedWord[index])
+                        return false;
+                }
+            return true;
+        }
+        #endregion
+        #region Enter number division
+        private static void EnterNumberDivision()
+        {
+            while (true)
+            {
+                if (ReadConsoleWord("Continue? Insert n to leave") == "n")
+                    break;
+                else
+                {
+                    int dividend = int.Parse(ReadConsoleWord("Insert dividend"));
+                    int divisior = int.Parse(ReadConsoleWord("Insert divisor"));
+                    Console.WriteLine($"The result from the division {dividend} / {divisior} equals {EnterDivision(dividend,divisior)}");
+                }
+            }
+        }
+        private static int EnterDivision(int dividend, int divisor) 
+        {
+            int result=0;
+            while (divisor <= dividend)
+            {
+                dividend = dividend - divisor;
+                result++;
+            }
+            
+            return result;
+        }
+        #endregion
+        #region Calculate pow
+        private static void CalculatePowExercice()
+        {
+            while (true)
+            {
+                if (ReadConsoleWord("Continue? Insert n to leave") == "n")
+                    break;
+                else
+                {
+                    int number = int.Parse(ReadConsoleWord("Write the number you want to pow."));
+                    int pow = int.Parse(ReadConsoleWord("Write the pow value"));
+                    Console.WriteLine($"The number {number} to the pow of {pow} is {calculatePow(number,pow)}");
+                }
+            }
+        }
+        private static int calculatePow(int number,int pow)
+        {
+            int result = 1;
+            for (int index = 0; index < pow; index++)
+            {
+                result=result*number;
+            }        
+            return result;
+        }
+        #endregion
         #region Palindrome
         private static void StartingPalindromeExcercice()
         {
@@ -84,24 +181,24 @@ namespace ConsoleApp1
                     DateTime date = GetDate();
                     Console.WriteLine("Insert date to compare");
                     DateTime dateToComparte = GetDate();
-                    eCompare comparation = CompareDate(date, dateToComparte);
-                    if (comparation == eCompare.IsBigger)
+                    @bool comparation = CompareDate(date, dateToComparte);
+                    if (comparation == @bool.IsBigger)
                         Console.WriteLine($"Date {date} is bigger than {dateToComparte}");
-                    else if (comparation == eCompare.IsLower)
+                    else if (comparation == @bool.IsLower)
                         Console.WriteLine($"Date {date} is lower than {dateToComparte}");
-                    else if (comparation == eCompare.IsEquals)
+                    else if (comparation == @bool.IsEquals)
                         Console.WriteLine($"Date {date} is equals than {dateToComparte}");
                 }
             }
         }
-        private static eCompare CompareDate(DateTime date, DateTime dateToCompare)
+        private static @bool CompareDate(DateTime date, DateTime dateToCompare)
         {
-            eCompare result = CompareInt(date.Year, dateToCompare.Year);
+            @bool result = CompareInt(date.Year, dateToCompare.Year);
 
-            if (result == eCompare.IsEquals)
+            if (result == @bool.IsEquals)
             {
                 result = CompareInt(date.Month, dateToCompare.Month);
-                if (result == eCompare.IsEquals)
+                if (result == @bool.IsEquals)
                     result = CompareInt(date.Day, dateToCompare.Day);
             }
 
@@ -118,7 +215,6 @@ namespace ConsoleApp1
             return date;
         }
         #endregion
-
         #region EjercicioLowerCamel
         private static void StartExerciceLowerCamelCase()
         {
@@ -176,7 +272,7 @@ namespace ConsoleApp1
         }
         #endregion
         #region Tools
-        public enum eCompare
+        public enum @bool
         {
             IsLower=1,
             IsBigger=2,
@@ -187,16 +283,16 @@ namespace ConsoleApp1
             Console.WriteLine(text);
             return Console.ReadLine();
         }
-        private static eCompare CompareInt(int value, int valueToCompare)
+        private static @bool CompareInt(int value, int valueToCompare)
         {
             if (value > valueToCompare)
-                return eCompare.IsBigger;
+                return @bool.IsBigger;
             else
             {
                 if (value == valueToCompare)
-                    return eCompare.IsEquals;
+                    return @bool.IsEquals;
                 else
-                    return eCompare.IsLower;
+                    return @bool.IsLower;
             }
         }
 
