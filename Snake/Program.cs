e5a427885ba98326dfec29e30ad633b8ce8b6b7a
mati <mati@DESKTOP-LBORILJ>
2024-09-09 14:01:06 +0200

    Snake

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,11 +8,42 @@ namespace Snake
 {
     internal class Program
     {
+       private enum eCellStatus
+        {
+            Empty,
+            Head,
+            Body,
+            BodyHead,
+            Fruit,
+        }
+
+        private static readonly IEnumerable<IEnumerable<eCellStatus>> board = new List<IEnumerable<eCellStatus>>();
         static void Main(string[] args)
         {
             while (true)
             {
                 
+            }
+        }
+
+        private void ShowBoar()
+        {
+            foreach (IEnumerable<eCellStatus> list in board)
+            {
+                foreach (eCellStatus eCellStatus in list)
+                {
+                    string line="";
+                    switch (eCellStatus)
+                    {
+                        case eCellStatus.Empty:
+                            line += " ";
+                            break;
+                            default:
+                            line += " ";
+                            break;
+                    }
+                }
+                Console.WriteLine("");
             }
         }
     }
