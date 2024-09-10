7192322524b7190683a243ef31ed471760e3001e
mati <mati@DESKTOP-LBORILJ>
2024-09-10 13:52:10 +0200

    Snake parcial

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake
 {
@@ -13,38 +13,207 @@ namespace Snake
             Empty,
             Head,
             Body,
-            BodyHead,
             Fruit,
         }
+        private static (int Column, int Line) _currentHeadPosition;
 
-        private static readonly IEnumerable<IEnumerable<eCellStatus>> board = new List<IEnumerable<eCellStatus>>();
+        private class Cell
+        {
+            public int Column { get; set; }
+            public int Line { get; set; }
+            public eCellStatus CellStatus { get; set; }
+
+        }
+
+        private static List<Cell> _snake = new List<Cell>();
+        private static eCellStatus[,] _board = new eCellStatus[10,10];
         static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            InsertSnake();
+            GenerateFruit();
+            while (true)
+            {
+                ShowBoard();
+                Movement();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Movement()
         {
             while (true)
             {
-                
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                (int, int) oldHeadPosition = _currentHeadPosition;
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.W:
+                        _currentHeadPosition.Line--;
+                        break;
+                    case ConsoleKey.A:
+                        _currentHeadPosition.Column--;
+                        break;
+                    case ConsoleKey.S:
+                        _currentHeadPosition.Line++;
+                        break;
+                    case ConsoleKey.D:
+                        _currentHeadPosition.Column++;
+                        break;
+                    default:
+                        Console.WriteLine("Tecla no asignada.");
+                        break;
+                }
+
+                if (_currentHeadPosition!=oldHeadPosition)
+                    break;
+
+                Console.WriteLine("Invalid key, use wasd");
+            }
+
+            ProcessNewPosition();
+        }
+
+        private static void ProcessNewPosition()
+        {
+            try
+            {
+                switch (_board[_currentHeadPosition.Line, _currentHeadPosition.Column])
+                {
+                    case eCellStatus.Body:
+                        End();
+                        break;
+                    case eCellStatus.Fruit:
+                        AddBody();
+                        MoveSnake();
+                        GenerateFruit();
+                        break;
+                    default:
+                        MoveSnake();
+                        break;
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                End();
             }
         }
 
-        private void ShowBoar()
+        private static void GenerateFruit()
         {
-            foreach (IEnumerable<eCellStatus> list in board)
+            Random random = new Random();
+            while (true)
             {
-                foreach (eCellStatus eCellStatus in list)
+               int randomLine = random.Next(_board.GetLength(0));
+               int randomColumn = random.Next(_board.GetLength(1));
+               if (_board[randomLine,randomColumn]==eCellStatus.Empty)
+               {
+                    _board[randomLine,randomColumn] = eCellStatus.Fruit;
+                    break;
+               }
+            }
+        }
+
+        private static void MoveSnake()
+        {
+            for (int i = _snake.Count-1; i >= 0 ; i--)
+            {
+                Cell cell = _snake[i];
+                (int Line, int Column) oldPosition = (cell.Line, cell.Column);
+                if (cell.CellStatus==eCellStatus.Head)
                 {
-                    string line="";
-                    switch (eCellStatus)
+                    cell.Column = _currentHeadPosition.Column;
+                    cell.Line= _currentHeadPosition.Line;
+                }
+                else
+                {
+                    cell.Column = _snake[i -1].Column;
+                    cell.Line = _snake[i -1].Line;
+                }
+                _board[cell.Line, cell.Column] = cell.CellStatus;
+                if (i == _snake.Count - 1 && (cell.Column != oldPosition.Column || cell.Line != oldPosition.Line))
+                    _board[oldPosition.Line, oldPosition.Column] = eCellStatus.Empty;
+
+            }
+        }
+        private static void AddBody()
+        {
+            Cell cell = new Cell();
+            Cell tail = _snake.Last();
+            cell.Column= tail.Column;
+            cell.Line= tail.Line;
+            _snake.Add(cell);
+        }
+
+        private static void End()
+        {
+            Console.WriteLine("Colision");
+        }
+        private static void InsertSnake()
+        {
+            int startPosition = _board.GetLength(0) / 2;
+            Cell cell = new Cell();
+
+            _currentHeadPosition = (startPosition, startPosition);
+            cell.CellStatus = eCellStatus.Head;
+            cell.Line = _currentHeadPosition.Line;
+            cell.Column = _currentHeadPosition.Column;
+            _snake.Add(cell);
+            _board[cell.Line, cell.Column] = cell.CellStatus;
+            for (int i = 1; i < 3; i++)
+            {
+                cell = new Cell();
+
+                cell.CellStatus = eCellStatus.Body;
+                cell.Line = _currentHeadPosition.Line + i;
+                cell.Column = _currentHeadPosition.Column;
+                _snake.Add(cell);
+                _board[cell.Line, cell.Column] = cell.CellStatus;
+            }
+        }
+        private static void ShowBoard()
+        {
+            ShowBoardLines();
+            for (int line = 0; line < _board.GetLength(0); line++)
+            {
+                string stringLine = "";
+
+                for (int column = 0; column < _board.GetLength(1); column++)
+                {
+                    switch (_board[line,column])
                     {
                         case eCellStatus.Empty:
-                            line += " ";
+                            stringLine += " ";
                             break;
-                            default:
-                            line += " ";
+                        case eCellStatus.Head:
+                            stringLine += "D";
+                            break;
+                        case eCellStatus.Body:
+                            stringLine += "=";
+                            break;
+                        case eCellStatus.Fruit:
+                            stringLine += "F";
+                            break;
+                        default:
+                            stringLine += " ";
                             break;
                     }
                 }
-                Console.WriteLine("");
+                Console.WriteLine($"|{stringLine}|");
+                ShowBoardLines();
             }
+        }
+
+        private static void ShowBoardLines()
+        {
+            for (int i = 0; i < _board.GetLength(0); i++)
+            {
+                Console.Write("_");
+            }
+
+            Console.WriteLine("__");
         }
     }
 }
---
+++ Snake2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Snake2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Snake2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("3ba7d72b-53c4-44a2-a437-e2bb1fb6647e")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
