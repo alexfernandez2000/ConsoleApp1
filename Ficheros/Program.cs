b4342b01558e2e44de4512330d8f85c24fca5a7c
alexFernandez <70752172+alexfernandez2000@users.noreply.github.com>
2024-09-15 10:26:03 +0200

    Files

--- Ficheros/Program.cs
+++ Ficheros/Program.cs
@@ -1,23 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Ficheros
 {
     internal class Program
     {
-        Dictionary<string,int>_teams=new Dictionary<string,int>();
+       private static Dictionary<string,int>_teams=new Dictionary<string,int>();
+        private static string _path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+@"\teams.txt";
         static void Main(string[] args)
         {
-            while (true)
+            int option = -1;
+            LoadTeams();
+            while (option!=0)
             {
-                switch (expression)
+                Console.WriteLine(@"Opciones:
+1:Añadir equipo
+2: Modificar puntuación
+3: Eliminar equipo
+4: Mostrar equipos
+Otro: Salir");
+                switch (GetInt())
                 {
-                    
+                    case 1:
+                        CreateNewTeam();
+                        SaveTeams();
+                        break;
+                    case 2:
+                        UpdateTeam();
+                        SaveTeams();
+                        break;
+                    case 3:
+                        DeleteTeam();
+                        SaveTeams();
+                        break;
+                    case 4:
+                        ShowTeams();
+                        break;
+                        default:
+                        option = 0;
+                        break;
                 }
             }
+        }
+
+        private static void ShowTeams()
+        {
+            foreach (var team in _teams)
+                Console.WriteLine(team);
+        }
+
+        private static void DeleteTeam()
+        {
+            string teamName = ReadConsoleWord("Escriba el nombre del equipo a eliminar");
+
+            if (_teams.ContainsKey(teamName))
+                _teams.Remove(teamName);
+            else
+                Console.WriteLine("Team not found");
+        }
+
+        private static void UpdateTeam()
+        {
+            string teamName = ReadConsoleWord("Escriba el nombre del equipo a modificar");
+
+            if (_teams.ContainsKey(teamName))
+            {
+                Console.WriteLine("Puntuación:");
+                _teams[teamName] = GetInt();
+            }
+            else
+                Console.WriteLine($"Team {teamName} not found");
+        }
+
+        private static void CreateNewTeam()
+        {
+            string teamName = ReadConsoleWord("Escribe nombre del equipo");
+            Console.WriteLine("Puntuación:");
+            int score = GetInt();
+
+            _teams.Add(teamName,score);
+        }
+        private static string ReadConsoleWord(string text)
+        {
+            Console.WriteLine(text);
+            return Console.ReadLine();
         }
         private static int GetInt()
         {
@@ -29,6 +101,19 @@ namespace Ficheros
                 Console.WriteLine("Invalid number");
             }
         }
-
+        private static void LoadTeams()
+        {
+            if (File.Exists(_path))
+            {
+                string jsonString = File.ReadAllText(_path);
+                Dictionary<string, int> teams = JsonSerializer.Deserialize<Dictionary<string, int>>(jsonString);
+                _teams = teams;
+            }
+        }
+        private static void SaveTeams()
+        {
+            string jsonString = JsonSerializer.Serialize(_teams);
+            File.WriteAllText(_path, jsonString);
+        }
     }
 }
