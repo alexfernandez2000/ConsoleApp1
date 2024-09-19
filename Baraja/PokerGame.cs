25761eed17a5d2919b5985d61817643089664c14
alexfernandez2000 <alexfernandezromero1@gmail.com>
2024-09-19 13:57:49 +0200

    Estructura

--- Baraja/PokerGame.cs
+++ Baraja/PokerGame.cs
@@ -8,9 +8,37 @@ namespace Baraja
 {
     internal class PokerGame
     {
+        private List<PokerPlayer>_players=new List<PokerPlayer>();
+        private Deck _mainDeck=new Deck().PokerDeck();
+        private readonly int _startMoney = 100;
         private void StartGame()
         {
+            _mainDeck.MixDeck();
+            CreatePlayers();
+            StartHand();
+        }
 
+        private void StartHand()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                StartRound();
+            }
+            
+        }
+
+        private void StartRound()
+        {
+            foreach (var pokerPlayer in _players)
+            {
+                Console.WriteLine();
+            }
+        }
+
+
+        private void CreatePlayers()
+        {
+            throw new NotImplementedException();
         }
     }
 }
