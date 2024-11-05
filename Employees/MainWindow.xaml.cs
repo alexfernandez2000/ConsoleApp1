d7cef0b8c5d0fbc33dd25d2c85845b2e8f8c3586
alexFernandez <70752172+alexfernandez2000@users.noreply.github.com>
2024-11-05 09:51:12 +0100

    Connection string

---
+++ Employees/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Employees
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = "Server=85.208.21.117,54321;Database=AlexFerEmployees;User Id=as;Password=Sql#123456789;";
+
+        }
+    }
+}
